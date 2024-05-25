b9dd71c67a02447827d4bc1afdf6eb4cd0a19e9e
CEPS0501 <christianepasquierino@yahoo.com>
2024-05-25 14:57:01 -0600

    actualizacion sesion2

--- S2Ejercicio1/app/build.gradle.kts
+++ S2Ejercicio1/app/build.gradle.kts
@@ -30,7 +30,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
 }
+
+
+
 
 dependencies {
 
--- S2Ejercicio1/app/src/main/java/com/example/s2_ejercicio1/MainActivity.kt
+++ S2Ejercicio1/app/src/main/java/com/example/s2_ejercicio1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.s2_ejercicio1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.s2_ejercicio1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        var name:String = "Pasquier"
+        var number1:Float = 3.0f
+        var number2:Double = 3.0
+        var number3:Int = 3
+
+        val city = "Managua"
+        start()
+
+    }
+
+    private fun start() {
+        binding.BtnShow.setOnClickListener{
+            var name = binding.EtName.text.toString()
+            Toast.makeText(this, "Hola $name, bienvenido a la Univalle", Toast.LENGTH_LONG).show()
+        }
     }
 }
