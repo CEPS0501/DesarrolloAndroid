93b56193af5c664e6e4b7f48d77aa6744099f417
CEPS0501 <christianepasquierino@yahoo.com>
2024-06-01 15:03:50 -0600

    Sesion3 Ejercicio

---
+++ S3Tarea1/app/src/main/java/com/example/s3_tarea1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.s3_tarea1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.service.voice.VoiceInteractionSession.ActivityId
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.s3_tarea1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+        private lateinit var binding : ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            binding.BtnEvaluar.setOnClickListener(){
+                var num = binding.TFNumber.editText.toString().toDouble()
+                var msn = isPositive(num)
+                Toast.makeText(this, msn, Toast.LENGTH_LONG).show()
+            }
+
+    }
+    fun isPositive(number:Double):String{
+        return if(number > 0)  "El $number es Positivo"
+        else if (number < 0) "El $number es Negativo"
+        else "El $number es Neutro"
+    }
+}
